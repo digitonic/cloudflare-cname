26ffe47702234995e02074b6031c64b484883c88
falko189 <daniele@digitonic.co.uk>
2021-05-18 12:45:47 +0200

    Added delete feature
    Signed-off-by: falko189 <daniele@digitonic.co.uk>

--- main.sh
+++ main.sh
@@ -7,7 +7,6 @@ record_domain=$RECORD_DOMAIN
 record_name=$RECORD_NAME
 REGEX='^([^.]+)'
 
-deployment=$DEPLOYMENT
 namespace=$NAMESPACE
 
 error_exit()
@@ -17,13 +16,45 @@ error_exit()
   exit 1
 }
 
+run_delete_record()
+{
+  # Seek for the record
+  record=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records?name=$record_search" -H "X-Auth-Email: $auth_email" -H "X-Auth-Key: $auth_key" -H "Content-Type: application/json")
+
+  # Set therecord identifier from result
+  record_identifier=$(echo "$record" | grep -Po '(?<="id":")[^"]*' | head -1)
+  echo "[Cloudflare] Record id found: $record_identifier"
+  # The execution of delete
+  delete=$(curl -s -X DELETE "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/$record_identifier" -H "X-Auth-Email: $auth_email" -H "X-Auth-Key: $auth_key" -H "Content-Type: application/json")
+
+  # The moment of truth
+  if [[ $delete == *"\"success\":false"*  ]]; then
+    error_exit "[Cloudflare] Delete failed. DUMPING RESULTS:\n $delete"
+  else
+    echo -e  "[Cloudflare] Record has been deleted from Cloudflare.\n https://$record_search \n"
+    exit 0
+  fi
+}
+
+
+if [ -z "${DEPLOYMENT_TARGET}" ];
+then
+  echo "[Cloudflare] DEPLOYMENT_TARGET is unset so DEPLOYMENT will be used: '$DEPLOYMENT'";
+  deployment_target=$DEPLOYMENT
+else
+  echo "[Cloudflare] DEPLOYMENT_TARGET is set to '$DEPLOYMENT_TARGET' and this will be used";
+  deployment_target=$DEPLOYMENT_TARGET
+fi
+
 echo "[Cloudflare] Move to the right namespace"
 
 kubectl config use-context "$KUBE_CONTEXT"
 
 echo "[Cloudflare] Get the LoadBalancer"
 
-elb_value=$(kubectl get service -n $namespace $deployment -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
+echo "kubectl get service -n $namespace $deployment_target -o jsonpath='{.status.loadBalancer.ingress[0].hostname}'"
+
+elb_value=$(kubectl get service -n $namespace $deployment_target -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
 
 echo $elb_value
 
@@ -41,13 +72,20 @@ echo "[Cloudflare] Name of the record: $matched"
 
 echo "[Cloudflare] Generated record content is : $record_search"
 
-# Seek for therecord
+if [[ $DELETE_RECORD == 1 ]]; then
+  run_delete_record
+  exit 0
+else
+  echo "[Cloudflare] Skipping DELETE_RECORD: $DELETE_RECORD !"
+fi
+
+# Seek for the record
 record=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records?name=$record_search" -H "X-Auth-Email: $auth_email" -H "X-Auth-Key: $auth_key" -H "Content-Type: application/json")
 
 # Can't do anything without both record
 if [[ $record == *"\"count\":0"* ]]; then
   create=$(curl -s -X POST "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records" -H "X-Auth-Email: $auth_email" -H "X-Auth-Key: $auth_key" -H "Content-Type: application/json" --data "{\"type\":\"CNAME\",\"proxied\":true,\"name\":\"$matched\",\"content\":\"$record_value\"}")
-  # Themoment of truth
+  # The moment of truth
   if [[ $create == *"\"success\":false"* ]]; then
     error_exit "[Cloudflare] Creation failed. DUMPING RESULTS:\n$create"
   else
@@ -59,10 +97,10 @@ fi
 # Set therecord identifier from result
 record_identifier=$(echo "$record" | grep -Po '(?<="id":")[^"]*' | head -1)
 echo "[Cloudflare] Record id found: $record_identifier"
-# Theexecution of update
+# The execution of update
 update=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/$record_identifier" -H "X-Auth-Email: $auth_email" -H "X-Auth-Key: $auth_key" -H "Content-Type: application/json" --data "{\"type\":\"CNAME\",\"proxied\":true,\"name\":\"$matched\",\"content\":\"$record_value\"}")
 
-# Themoment of truth
+# The moment of truth
 if [[ $update == *"\"success\":false"*  ]]; then
   error_exit "[Cloudflare] Update failed. DUMPING RESULTS:\n $update"
 else
